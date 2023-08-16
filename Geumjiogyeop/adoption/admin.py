cc946a2fd23da1d4b23e2525e20234366362aed8
Yaeran Kim <yaerankim000812@gmail.com>
2023-08-16 14:52:27 +0900

    admin 추가

--- Geumjiogyeop/adoption/admin.py
+++ Geumjiogyeop/adoption/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Adoption)
+class AdoptionModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserLikedAdoption)
+class UserLikedAdoptionModelAdmin(admin.ModelAdmin):
+    pass

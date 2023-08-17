0b35fca7c3fa122a69cd7c266b4d8be8c0c1f27d
ljy6712 <a01045386712@gmail.com>
2023-08-17 18:07:27 +0900

    신고접수 이름 추가

--- Geumjiogyeop/reports/models.py
+++ Geumjiogyeop/reports/models.py
@@ -9,9 +9,9 @@ class Report(models.Model):
         ("가족","2"),
         ("기타","3"),
     ]
+    name = models.TextField(verbose_name= "사고자성함",default="")
     relation = models.CharField(verbose_name = "관계", max_length=2, choices = RELATION_CHOICES)
     victim = models.ForeignKey(to=User, on_delete = models.CASCADE)
     date = models.DateTimeField("사고날짜")
     type = models.TextField(verbose_name = "유형")
     etc = models.TextField(verbose_name="기타이유", default="")
-

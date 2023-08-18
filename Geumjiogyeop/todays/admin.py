3bd97f1f9aad3d1189add3c448ec81b586df17fb
ljy6712 <a01045386712@gmail.com>
2023-08-18 23:34:56 +0900

    admin

--- Geumjiogyeop/todays/admin.py
+++ Geumjiogyeop/todays/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import *
+from .models import Today, Images
 
 # Register your models here.
 @admin.register(Today)
 class TodayModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.reister(Images)
+class ImageModelAdmin(admin.ModelAdmin):
     pass
